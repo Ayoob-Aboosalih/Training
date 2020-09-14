7b0d7fa542b07e638c564529ea3545ea4a236ca2
Ayoob-Aboosalih <67360797+Ayoob-Aboosalih@users.noreply.github.com>
2020-09-14 18:12:13 +0530

    start

---
+++ web_template/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Webpage from './components/Webpage';
+
+
+function App() {
+  return (
+    <div style={{backgroundImage: "lightblue"}}> 
+      <Webpage />
+    </div>
+  );
+}
+
+export default App;
---
+++ web_template/src/components/Webpage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import '../css/Webpage.css'
+
+export default function webpage() {
+  return (
+    <div >
+      <div id='header' >
+        <header>
+          <h1>home-page-header</h1>
+        </header>
+      </div>
+      <div class='container'>
+      <div class='menu'>
+        <p>home-page-menu</p>
+      </div>
+      <div class='details'>
+        <p>home-page-details</p>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+
+
